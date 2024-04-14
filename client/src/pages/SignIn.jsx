da86187faa84d51c1cfd2757fa4491d073ab3396
Duy <duyvu372001@gmail.com>
2024-04-14 12:44:25 +0900

    completeSignIn

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,101 +1,89 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const res = await fetch('/api/auth/sign-in', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await res.json();
+
+            if (data.success === false) {
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+
+            setLoading(false);
+            setError(null);
+            navigate('/');
+        } catch (error) {
+            setLoading(false);
+            setError(error.message);
+        }
+    };
 
     return (
-        <>
-            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
-                    </h2>
-                </div>
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST">
-                        <div>
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                                Email address
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    className="border p-3 rounded-lg"
+                    id="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    className="border p-3 rounded-lg"
+                    id="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+                <button
+                    disabled={loading}
+                    className="flex w-full justify-center bg-slate-700 text-white p-3 rounded-lg 
+                            font-semibold uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                    {loading ? 'Loading...' : 'Sign In'}
+                </button>
+                <button
+                    className="bg-red-700 text-white p-3 rounded-lg uppercase 
+                hover:opacity-95"
+                >
+                    Continue with google
+                </button>
+            </form>
 
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label
-                                    htmlFor="password"
-                                    className="block text-sm font-medium leading-6 text-gray-900"
-                                >
-                                    Password
-                                </label>
-                                <div className="text-sm">
-                                    <a
-                                        href="#"
-                                        className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                    autoComplete="current-password"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
-
-                    <p className="mt-10 text-center text-sm text-gray-500">
-                        Don't Have an account?{' '}
-                        <Link
-                            to="/sign-up"
-                            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                        >
-                            Sign Up
-                        </Link>
-                    </p>
-                </div>
+            <div className="flex gap-2 mt-5 ">
+                <p>Don't have a account?</p>
+                <Link to="/sign-up">
+                    <span className="text-blue-700">Sign Up</span>
+                </Link>
             </div>
-        </>
+            {error && <p className="text-red-500 mt-5">{error} </p>}
+        </div>
     );
 }
