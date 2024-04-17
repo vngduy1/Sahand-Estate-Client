fab5448fc0b61a24f574505a4a7f9c99655203b3
Duy <duyvu372001@gmail.com>
2024-04-17 12:22:08 +0900

    Complete_image_upload

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { SignUp, signIn } from "../controllers/authController.js";
+import { SignUp, google, signIn } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", SignUp);
 router.post("/sign-in", signIn);
+router.post("/google", google);
 
 export default router;
